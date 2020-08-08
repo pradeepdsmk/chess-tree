d3d9a11d5f169e9e0cd90be2bc5f2c4708290c46
pradeepdsmk <pradeepdsmk@gmail.com>
2020-08-08 07:46:31 +0530

    Tree class added for move tree building

---
+++ Tree.cpp
@@ -0,0 +1,92 @@
+#include "Tree.h"
+
+namespace chess {
+
+    TreeNode::TreeNode() {
+        move = nullptr;
+        left = nullptr;
+        right = nullptr;
+        thisTree = nullptr; // will be set by TreeNode
+        children = nullptr;
+    }
+    TreeNode::TreeNode(Move* _move) : TreeNode() {
+        move = _move;
+    }
+
+    void TreeNode::setChildren(Tree* _children) {
+        children = _children;
+        children->parent = this;
+    }
+
+    TreeNode::~TreeNode() {
+        if (move) {
+            delete move;
+            move = nullptr;
+        }
+        if (children) {
+            delete children;
+            children = nullptr;
+        }
+    }
+
+
+    /////////////////////////////
+
+
+    Tree::Tree() {
+        tree = new TreeNode();
+        last = tree;
+        parent = nullptr;
+        length = 0;
+    }
+
+    Tree::~Tree() {
+        while (last->left) {
+            last = last->left;
+
+            delete last->right;
+            last->right = nullptr;
+        }
+
+        delete tree;
+        tree = nullptr;
+        //parent = nullptr;
+        //last = nullptr;
+        //length = 0;
+    }
+
+    void Tree::add(Move* move) {
+        TreeNode* treeNode = new TreeNode(move);
+        treeNode->thisTree = this;
+        treeNode->left = last;
+        last->right = treeNode;
+        last = treeNode;// move last after setting last right
+        ++length;
+    }
+
+    TreeNode* Tree::remove(TreeNode* p) {
+        TreeNode* pprev = p->left;
+        p->left->right = p->right;
+        if (p->right) {
+            p->right->left = p->left;
+        }
+        else {
+            // this is last node
+            last = last->left;
+        }
+        delete p;
+        p = nullptr;
+        --length;
+
+        return pprev;
+    }
+
+    TreeNode* Tree::getAt(int i) {
+        // i is 0 based index
+        TreeNode* p = tree->right;
+        while (i--) {
+            p = p->right;
+        }
+        return p;
+    }
+}
---
+++ Tree.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Move.h"
+
+namespace chess {
+	
+    struct Tree;
+    class Move;
+    class Square;
+
+    struct TreeNode {
+
+        Move* move;
+
+        TreeNode* left;
+        TreeNode* right;
+
+        Tree* thisTree;
+        Tree* children;
+
+        TreeNode();
+        TreeNode(Move* move);
+        void setChildren(Tree* _children);
+        ~TreeNode();
+    };
+
+    struct Tree {
+
+        TreeNode* tree;
+        TreeNode* last;
+        TreeNode* parent;
+        unsigned length;
+
+        Tree();
+        void add(Move* move);
+        TreeNode* remove(TreeNode* p);
+        TreeNode* getAt(int i);
+        ~Tree();
+    };    	
+}
+
